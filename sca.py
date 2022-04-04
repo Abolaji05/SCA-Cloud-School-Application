0fd9496fc42a258e6541f1fc747143600c32d55d
Abolaji05 <102436106+Abolaji05@users.noreply.github.com>
2022-04-04 16:12:02 -0400

    Update sca.py

--- sca.py
+++ sca.py
@@ -1,4 +1,4 @@
 Name = "Abolaji Adeyi"
 Application = "Cloud Engineering"
 School = "She Codes Africa Cohort-3"
-print(f"I, {Name}, will be joining the {Application} {School} class in 2022!")
+print(f"I, {Name}, will be joining the {Application} {School} class in 2022")
